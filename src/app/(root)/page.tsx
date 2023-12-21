405b5a9628aba825b89b0e528fb52a1af6c41ebb
Teagen Kiel <teagen.kiel@atomicobject.com>
2023-12-21 15:15:34 -0600

    Try fallback

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,6 +1,12 @@
 import { fetchDeviceData } from "@/db/actions/device-data.actions";
+import { useRouter } from "next/router";
 
 export default async function Home() {
+  const router = useRouter();
+  if (router.isFallback) {
+    return <div>Loading...</div>;
+  }
+
   const deviceData = await fetchDeviceData();
   return (
     <div className="grow">
@@ -19,12 +25,11 @@ export default async function Home() {
 }
 
 // This function gets called at build time
-// export async function getStaticPaths() {
-//   return {
-//     // Only `/posts/1` and `/posts/2` are generated at build time
-//     paths: [{ params: { id: "1" } }, { params: { id: "2" } }],
-//     // Enable statically generating additional pages
-//     // For example: `/posts/3`
-//     fallback: true,
-//   };
-// }
+export async function getStaticPaths() {
+  return {
+    // Only `/posts/1` and `/posts/2` are generated at build time
+    // Enable statically generating additional pages
+    // For example: `/posts/3`
+    fallback: true,
+  };
+}
