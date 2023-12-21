2af7adf4828c87cd5b67e6a88e4c166daf82ccd0
Teagen Kiel <teagen.kiel@atomicobject.com>
2023-12-21 15:40:02 -0600

    Add console logs

--- src/db/actions/device-data.actions.ts
+++ src/db/actions/device-data.actions.ts
@@ -24,8 +24,10 @@ export const pushDeviceData = async (input: {
 };
 
 export const fetchDeviceData = async (): Promise<DeviceData[]> => {
+  console.log("rendering fetch device data");
   await connectToDB();
 
+  console.log("rendering fetch device data: db connected");
   const deviceData = await DeviceDataModel.find();
   return deviceData;
 };
