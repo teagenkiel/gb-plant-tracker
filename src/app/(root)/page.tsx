9765673352b6919d6c3fd845d631bd2217b402b5
Teagen Kiel <teagen.kiel@atomicobject.com>
2023-12-29 00:07:53 -0600

    Add search params

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,8 +1,12 @@
 import { fetchDeviceData } from "@/db/actions/device-data.actions";
 
-export default async function Home() {
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | undefined };
+}) {
   const deviceData = await fetchDeviceData();
-
+  console.log(deviceData);
   return (
     <div className="grow">
       <h1 className="text-white">Device Data</h1>
