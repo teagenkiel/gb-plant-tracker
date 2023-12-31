10594b84e543cad533761194fc4dad1e4e25a63c
Teagen Kiel <teagen.kiel@atomicobject.com>
2023-12-31 01:17:00 -0600

    Update deviceName function

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,6 +1,4 @@
-import { DeviceName } from "@/types/types";
-
-export const deviceIdToName = (deviceId: String): DeviceName => {
+export const deviceIdToName = (deviceId: String): String => {
   switch (deviceId) {
     case "1318372":
       return "PLANT DADDY";
